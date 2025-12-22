081009b5f56184b392bec781544fdffb4a8017de
Derek Lim <dereklim3006@gmail.com>
2025-12-22 11:05:43 -0500

    style: ex4a - lay out shoe grid

--- src/components/ShoeCard/ShoeCard.jsx
+++ src/components/ShoeCard/ShoeCard.jsx
@@ -53,6 +53,7 @@ const ShoeCard = ({
 const Link = styled.a`
   text-decoration: none;
   color: inherit;
+  flex: 1 1 344px;
 `;
 
 const Wrapper = styled.article``;
@@ -61,9 +62,13 @@ const ImageWrapper = styled.div`
   position: relative;
 `;
 
-const Image = styled.img``;
+const Image = styled.img`
+  width: 100%;
+`;
 
 const Row = styled.div`
+  display: flex;
+  justify-content: space-between;
   font-size: 1rem;
 `;
 
